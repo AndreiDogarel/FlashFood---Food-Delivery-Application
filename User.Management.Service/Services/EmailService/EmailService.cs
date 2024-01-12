67ac3c8eebff2b452a5ad156eba3797ae8d7c949
Andrei Dogarel <75136445+AndreiDogarel@users.noreply.github.com>
2024-01-12 20:49:19 +0200

    Add files via upload

---
+++ User.Management.Service/Services/EmailService/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using User.Management.Service.Models;
+
+namespace User.Management.Service.Services.EmailService
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailConfiguration _emailConfiguration;
+        public EmailService(EmailConfiguration emailConfiguration) => _emailConfiguration = emailConfiguration;
+        public void SendEmail(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+            Send(emailMessage);
+        }
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("FlashFood~VerifyYourEmail", _emailConfiguration.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+
+            return emailMessage;
+        }
+        private void Send(MimeMessage mailMessage)
+        {
+            using var client = new SmtpClient();
+            try
+            {
+                client.Connect(_emailConfiguration.SmtpServer, _emailConfiguration.Port);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate(_emailConfiguration.UserName, _emailConfiguration.Password);
+
+                client.Send(mailMessage);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                client.Disconnect(true);
+                client.Dispose();
+            }
+        }
+    }
+}
